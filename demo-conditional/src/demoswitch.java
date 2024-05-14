09f5ed0aa12678425c1f0198afcd417c37fc8586
Andrew <andrew18523@gmail.com>
2024-05-14 21:48:43 +0800

    update

--- demo-conditional/src/demoswitch.java
+++ demo-conditional/src/demoswitch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class demoswitch {
   public static void main(String[] args){
     char grade ='B';
@@ -33,5 +35,11 @@ public class demoswitch {
         x +=10; 
     System.out.println(x); //30
     }
+    //Scanner
+    
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please input ta number: ");
+    int month = input.nextInt();//line of input ->int
+    System.out.println("month=" + month);
   }
 }
