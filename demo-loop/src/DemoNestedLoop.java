76afa18af5670bf84b84855de9040dba3fd41e42
Andrew <andrew18523@gmail.com>
2024-05-17 21:32:05 +0800

    update

---
+++ demo-loop/src/DemoNestedLoop.java
@@ -0,0 +1,79 @@
+public class DemoNestedLoop {
+  public static void main(String[] args) {
+    for (int i=0; i<4 ; i++){
+      for (int j=0; j<2;j++){
+        System.out.println("hello");
+      }
+    }
+    for (int i=0; i<4 ; i++){
+      for (int j=0; j<2;j++){
+        System.out.println("hello. "+"i="+i+" j="+j);
+      }
+    }
+    //hello. i=0 j=0
+    //hello. i=0 j=1
+    //hello. i=1 j=0
+    //hello. i=1 j=1
+    //hello. i=2 j=0
+    //hello. i=2 j=1
+    //hello. i=3 j=0
+    //hello. i=3 j=1
+
+    //output
+    //hello. i=1 j=0
+    //hello. i=3 j=0
+    for (int i=0; i<4 ; i++){
+      if (i%2!=0){
+        for (int j=0; j<2;j++){
+          if(j%2==0){
+            System.out.println("hello. "+"i="+i+" j="+j);
+          }
+        }
+      }
+    }
+    //Approcah 1:
+    for (int i=0; i<4 ; i++){
+      for (int j=0; j<2;j++){
+        if(i%2==1 && j%2==0){
+          System.out.println("hello1. "+"i="+i+" j="+j);
+        }
+      }
+    }
+    //Approach 2:
+    for (int i=0;i<4;i++){
+      if (i%2==0){
+        continue;
+      }
+      for (int j=0;j<2;j++){
+        if(j%2 !=0){
+          continue;
+        }
+        System.out.println("hello2. "+"i="+i+" j="+j);
+      }
+    }
+
+    // 1 -20
+    // 1 3 5 7 9 12 14 16 18 20
+
+    for (int i=0;i<21;i++){
+      if (i<=10 && i%2==0){
+        continue;
+      } else if (i>10 && i%2 != 0){
+        continue;
+      } else {
+        System.out.println(i);
+      } 
+    }
+
+
+    //Searching
+    String s= "Vincent Book Game Laptop";//13
+    //Find the first index of char'a'
+    for (int i=0; i<s.length();i++){
+      if(s.charAt(i)=='a'){
+        System.out.println("The first index of char'a' is "+i);
+        break; // break is exit for loop not if loop
+      }
+    }
+  }
+}
---
+++ demo-loop/src/demoloop.java
@@ -0,0 +1,90 @@
+public class demoloop {
+  public static void main(String[] args) {
+    int x=3;
+
+    //for loop
+    // initialization (varable declaration & assigment; continue criteria ; incremental opration)
+    //Step 1: "int i=0"
+    //Step 2: check if i<4? Yes
+    //Step 3: print hello
+    //Step 4: i++ -> i become 1
+    //Step 5: check if i<4? Yes
+    //Step 6: print hello
+    //Step 7: i++ -> i become 2
+    //Step 8: check if i<4? Yes
+    //Step 9: print hello
+    //Step 10: i++ -> i become 3
+    //Step 11: check if i<4? Yes
+    //Step 12: print hello
+    //Step 13: i++ -> i become 4 
+    //Step 14: check if i<4? No
+    //Step 15: Exit For loop
+    for (int i=0 ; i<4 ;i++) { //0,1,2,3
+      System.out.println("hello");
+    }
+    //hello
+    //hello
+    //hello
+    //hello
+    
+    //y=81
+    int y=3;
+    for (int i=0; i<3;i++){
+      y*=3;
+    }
+    System.out.println("y="+y);
+
+    // 2
+    // 4
+    // 6
+    for (int i=0; i<7;i++){
+      if(i !=0 && i%2 == 0){
+        System.out.println(i);
+      }
+    }
+    //  1 + 2 + 3 + ... + 10
+    int sum=0;
+    for (int i=0; i<11;i++){
+      sum += i;
+    }
+    System.out.println("sum="+sum); //55
+
+    int sum2=0;
+    for (int i=0; i<9; i++){
+      if (i !=0 && i%2==0){
+        sum2 +=i;
+      }
+    }
+    System.out.println("sum2="+sum2); //20
+    for (int i=8; i>0; i--){
+      if (i%2==0){
+        System.out.println(i);
+      }
+    }
+    int k=8;
+    for (int i=0; i<9;i++){
+      if ((k-i)%2==0 &&(k-i)!=0){
+        System.out.println(k-i);
+      }
+    }
+    //break
+    for (int i=0;i<10;i++){
+      if (i>7){
+        break; //exit loop
+      }
+      System.out.println(i);// main logic
+    }
+    //0
+    //1
+    //...
+    //7
+    int total=0;
+    for (int i=0; i<5; i++){
+      if (i>3){
+        continue; //if i>3 don't run main logic and go to i++
+      }
+      total+=i;
+    }
+    System.out.println("Total="+total);//6
+  }
+}
