dddfeb1dd38c7c3a544a9ae60f1886ca979dd9fd
Andrew <andrew18523@gmail.com>
2024-06-08 18:52:50 +0800

    update

--- exercise/src/Supermarket.java
+++ exercise/src/Supermarket.java
@@ -2,7 +2,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Supermarket {
-  
+  // 30% off discount will be given if the expired date is within 180 days.
+  // All expired item should be disposed and not sell to customer.
+
+
   private String itemName;
   private LocalDate expiryDate;
   private int quantity; 
@@ -54,41 +57,42 @@ public class Supermarket {
 
   public double discount (){
     if (this.expiryDate.isBefore(LocalDate.now())){
-      return 0;
+      return 0; // Item should be disposed as already expired
     }
     else if (LocalDate.now().plusDays(180L).isBefore(this.expiryDate) == false){
-      return 0.7;
+      return 0.7; // 30% off for item's expiry date within 180 days
     } else {
       return 1;
     }
   }
   public String totalPrice (int quantity, double discount){
     if (discount == 0){
-      return "Item already expired. Please dispose it instead of selling it.";
+      return "Item already expired. Please dispose it instead of selling it."; // Item should be disposed as already expired. No stock should be reduce.
     }
     else if (quantity> this.quantity){
-      return "Item quantity is less than requested quantity";
+      return "Item quantity is less than requested quantity"; // requested quantity is greater then item quantity.
     } else {
-      this.quantity -= quantity;
+      this.quantity -= quantity; // reduce the item quantity per transcation
       return "The total price is " + BigDecimal.valueOf(this.price).multiply(BigDecimal.valueOf(quantity)).multiply(BigDecimal.valueOf(discount)).doubleValue();
     }
   }
 
+  
   public static void main(String[] args) {
     Supermarket Chocolate = new Supermarket("Chocolate", LocalDate.of(2024,8,31), 100, 499);
     Supermarket Cola = new Supermarket("Cola", LocalDate.of(2025,12,31), 100, 12);
     Supermarket Chip = new Supermarket("Chip", LocalDate.of(2023,12,31), 100, 12);
-    System.out.println(Cola.toString());
-    System.out.println(Chocolate.toString());
-    System.out.println(Chip.toString());
-    System.out.println(Cola.totalPrice(10, Cola.discount()));
-    System.out.println(Cola.toString());
-    System.out.println(Chocolate.totalPrice(10, Chocolate.discount()));
-    System.out.println(Chocolate.toString());
-    System.out.println(Chocolate.totalPrice(100, Chocolate.discount()));
-    System.out.println(Chocolate.toString());
-    System.out.println(Chip.totalPrice(10, Chip.discount()));
-    System.out.println(Chip.toString());
+    System.out.println(Cola.toString()); // Cola has total 100 qty in stock. The price is 12.0. And the expiry date is 2025-12-31
+    System.out.println(Chocolate.toString()); // Chocolate has total 100 qty in stock. The price is 499.0. And the expiry date is 2024-08-31.
+    System.out.println(Chip.toString()); // Chip has total 100 qty in stock. The price is 12.0. And the expiry date is 2023-12-31.
+    System.out.println(Cola.totalPrice(10, Cola.discount())); // The total price is 120.0
+    System.out.println(Cola.toString()); // Remaining stock -> Cola has total 90 qty in stock. The price is 12.0. And the expiry date is 2025-12-31.
+    System.out.println(Chocolate.totalPrice(10, Chocolate.discount())); //Expiry Date is within 180 day from today. 30% off discount -> The total price is 3493.0
+    System.out.println(Chocolate.toString()); // Remaining stock ->  Chocolate has total 90 qty in stock. The price is 499.0. And the expiry date is 2024-08-31.
+    System.out.println(Chocolate.totalPrice(100, Chocolate.discount())); //Item quantity (90) is less than requested quantity (100)
+    System.out.println(Chocolate.toString()); // No stock is reduce from line 91. -> Chocolate has total 90 qty in stock. The price is 499.0. And the expiry date is 2024-08-31.
+    System.out.println(Chip.totalPrice(10, Chip.discount())); // Chip already expired. -> Item already expired. Please dispose it instead of selling it.
+    System.out.println(Chip.toString()); // Remaining stock -> Chip has total 100 qty in stock. The price is 12.0. And the expiry date is 2023-12-31.
 
   }
   
